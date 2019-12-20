8a9a59eeda6653875cff049dc4aa7a984632e290
Manuel Coenen <manuel.coenen@gmail.com>
2019-12-20 13:26:17 +0100

    Finish basic source code comments

--- machine/heat.go
+++ machine/heat.go
@@ -1,56 +1,102 @@
 package machine
 
+const (
+	// AbsoluteZero temperature in degC
+	AbsoluteZero = -273.15
+)
+
+// HeaterState represents available heater states
 type HeaterState uint64
 
 const (
+	// Off for a turned off heater
 	Off HeaterState = iota
+	// Standby for a heater in standby mode
 	Standby
+	// Active for an active heater
 	Active
+	// Tuning for a tuning heater
 	Tuning
+	// Offline for a heater that cannot be reached
 	Offline
 )
 
+// Heat holds information about the heat subsystem
 type Heat struct {
-	Beds              []BedOrChamber
-	Chambers          []BedOrChamber
+	// Beds is a list of configured beds
+	Beds []BedOrChamber
+	// Chambers is a list of configured chambers
+	Chambers []BedOrChamber
+	// ColdExtrusionTemp is the minimum temperature required for extrusion moves (in degC)
 	ColdExtrusionTemp float64
-	ColdRetractTemp   float64
-	Extra             []ExtraHeater
-	Heaters           []Heater
+	// ColdRetractTemp is the minimum temperature required for retraction moves (in degC)
+	ColdRetractTemp float64
+	// Extra is a list of configured extra heaters
+	Extra []ExtraHeater
+	// Heaters is a list of configured heaters
+	Heaters []Heater
 }
 
+// BedOrChamber holds information about a bed or a chamber heater
 type BedOrChamber struct {
-	Active  []float64
+	// Active temperatures (in degC)
+	Active []float64
+	// Standby temperatures (in degC)
 	Standby []float64
-	Name    string
+	// Name of the bed or chamber
+	Name string
+	// Heaters is a list of heater indices controlled by this bed or chamber
 	Heaters []int64
 }
 
+// ExtraHeater holds information about an extra heater (virtual)
 type ExtraHeater struct {
+	// Current temperature (in degC)
 	Current float64
-	Name    string
-	State   HeaterState
-	Sensor  int64
+	// Name of the extra heater
+	Name string
+	// State of the exra heater or nil if unknown/unset
+	State *HeaterState
+	// Sensor number of the extra heater or nil if unknown/unset
+	Sensor *int64
 }
 
+// Heater holds information about a heater
 type Heater struct {
+	// Current temperature (in degC)
 	Current float64
-	Name    string
-	State   HeaterState
-	Model   HeaterModel
-	Max     float64
-	Sensor  int64
+	// Name of the heater
+	Name string
+	// State of the heater
+	State HeaterState
+	// Model hold information about the heater model
+	Model HeaterModel
+	// Max allowed temperature of this heater (in degC)
+	Max float64
+	// Sensor number of this heater or nil if unknown
+	Sensor *int64
 }
 
+// HeaterModel holds information about the way a heater heats up
 type HeaterModel struct {
-	Gain            float64
-	TimeConstant    float64
-	DeadTime        float64
-	MaxPwm          float64
+	// Gain value or nil if unknown
+	Gain *float64
+	// TimeConstant or nil if unknown
+	TimeConstant *float64
+	// DeadTime of this heater or nil if unknown
+	DeadTime *float64
+	// MaxPwm value for this heater (0 if unknown)
+	MaxPwm float64
+	// StandardVoltage of this heater (0 if unknown)
 	StandardVoltage float64
-	UsePID          bool
-	CustomPID       bool
-	P               float64
-	I               float64
-	D               float64
+	// UsePID indicates usage of PID (instead of bang-bang)
+	UsePID bool
+	// CustomPID indicates the usage of custom PID values
+	CustomPID bool
+	// P is the proportional value of the PID regulator
+	P float64
+	// I is the integral value of the PID regulator
+	I float64
+	// D is the derivative value pf the PID regulator
+	D float64
 }
